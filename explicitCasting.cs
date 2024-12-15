7fa38d9ee822c88e30d747cf3b224434678ca535
LAPTOP <david.walter.marinescu@gmail.com>
2024-12-15 16:06:46 +0200

    Added Data Type Conversion Examples

---
+++ explicitCasting.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = 9.78;
+            int myInt = (int) myDouble;
+
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myInt);
+        }
+    }
+}
