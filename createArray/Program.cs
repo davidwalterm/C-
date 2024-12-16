c2fe0ffad850f4ba840441261d7e7cb0e1b7e6f2
LAPTOP <david.walter.marinescu@gmail.com>
2024-12-16 12:22:52 +0200

    Added Loops Examples

---
+++ createArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+            Console.WriteLine(cars[0]);
+        }
+    }
+}
